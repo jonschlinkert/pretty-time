973dcc84a5516666c58f197091c07b018c727b14
jonschlinkert <jon.schlinkert@sellside.com>
2015-10-12 05:01:49 -0400

    first release

--- index.js
+++ index.js
@@ -8,35 +8,58 @@
 'use strict';
 
 var isNumber = require('is-number');
-
+var utils = require('./utils');
 var scale = {
-  w: 6048e11,
-  d: 864e11,
-  h: 36e11,
-  m: 6e10,
-  s: 1e9,
-  ms: 1e6,
-  μs: 1e3,
-  ns: 1,
+  'w': 6048e11,
+  'd': 864e11,
+  'h': 36e11,
+  'm': 6e10,
+  's': 1e9,
+  'ms': 1e6,
+  'μs': 1e3,
+  'ns': 1,
 };
 
-function pretty(time, round) {
-  var num = (time[0] * 1e9) + time[1];
-  var res = '', uom;
-
-  for (uom in scale) {
-    var val = scale[uom];
-
-    var inc = +num / val;
-    if (inc >= 1) {
-      if (isNumber(round)) {
-        return inc.toFixed(round) + uom;
-      }
-      res += inc + uom + ' ';
-      num -= (inc * val);
-    }
+function pretty(time, smallest, digits) {
+  if (!Array.isArray(time) || time.length !== 2) {
+    throw new TypeError('expected an array from process.hrtime()');
   }
-  return res;
+
+  if (isNumber(smallest)) {
+    digits = smallest;
+    smallest = null;
+  }
+
+  var num = (+time[0] * 1e9) + +time[1];
+  var res = '';
+  var prev;
+
+  for (var uom in scale) {
+    var step = scale[uom];
+    var inc = num / step;
+
+    if (smallest && utils.isSmallest(uom, smallest)) {
+      inc = utils.round(inc, digits);
+      if (prev && (inc === (prev / step))) --inc;
+      res += inc + uom;
+      return res.trim();
+    }
+
+    if (inc < 1) continue;
+    if (!smallest) {
+      inc = utils.round(inc, digits);
+      res += inc + uom;
+      return res;
+    }
+
+    prev = step;
+
+    inc = Math.floor(inc);
+    num -= (inc * step);
+    res += inc + uom + ' ';
+  }
+
+  return res.trim();
 }
 
 /**
